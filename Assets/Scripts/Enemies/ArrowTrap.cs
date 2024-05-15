c475d689599368fb1bb66b166cda6a501f5967de
happyhzr <insisthzr@163.com>
2024-05-15 21:27:15 +0800

    add a arrow trap

---
+++ Assets/Scripts/Enemies/ArrowTrap.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrowtrap : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private float cooldownTimer;
+
+    private void Attack()
+    {
+        cooldownTimer = 0;
+
+        int i = FindFireball();
+        fireballs[i].transform.position = firePoint.position;
+        fireballs[i].GetComponent<EnemyProjectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+        if (cooldownTimer >= attackCooldown)
+        {
+            Attack();
+        }
+    }
+}
