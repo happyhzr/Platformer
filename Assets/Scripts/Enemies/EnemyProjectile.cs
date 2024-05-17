0f9c47d093d59e70e509f049cb09828cf969abe0
happyhzr <insisthzr@163.com>
2024-05-17 21:37:38 +0800

    add a spikehead

--- Assets/Scripts/Enemies/EnemyProjectile.cs
+++ Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -26,7 +26,7 @@ public class EnemyProjectile : EnemyDamage
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private new void OnTriggerEnter2D(Collider2D collision)
     {
         base.OnTriggerEnter2D(collision);
         gameObject.SetActive(false);
