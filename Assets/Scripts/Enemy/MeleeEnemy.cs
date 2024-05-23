9a26b2f184e6394ce43705d2553800d3fdd4c336
happyhzr <insisthzr@163.com>
2024-05-23 21:59:42 +0800

    add the enemy's patrol

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
+    [Header("Attack Parameters")]
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
-    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
     [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
 
     private Animator anim;
     private Health playerHealth;
 
+    private EnemyPatrol enemyPatrol;
+
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
     private void Update()
@@ -31,6 +39,11 @@ public class MeleeEnemy : MonoBehaviour
                 cooldownTimer = 0;
                 anim.SetTrigger("meleeAttack");
             }
+        }
+
+        if (enemyPatrol != null)
+        {
+            enemyPatrol.enabled = !PlayerInSight();
         }
     }
 
